dad0850edbfa23fa443cd3e4f4046a52a98eefa2
Elad Katz <eladk@seladom.sela.co.il>
2017-07-02 14:28:52 +0300

    finished demos 14

---
+++ Allfiles/20486c/Mod13/Democode/OperasWebsites_13_end/OperasWebsites/WebServices/QuotesService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="QuotesService.asmx.cs" Class="OperasWebsites.WebServices.QuotesService" %>
---
+++ Allfiles/20486c/Mod14/Democode/OperasWebsites_14_end/OperasWebsites/Models/Opera.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OperasWebsites.Models
+{
+    public class Opera
+    {
+        [Required]
+        public int OperaID { get; set; }
+        [StringLength(200)]
+        public string Title { get; set; }
+        [CheckValidYear]
+        [Required]
+        public int Year { get; set; }
+        public string Composer { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+
+
+    public class CheckValidYear : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if (year < 1598)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public CheckValidYear()
+        {
+            ErrorMessage = "The earliest opera is Daphne, 1598, by Corsi, Peri, and Rinuccini";
+        }
+    }
+}
