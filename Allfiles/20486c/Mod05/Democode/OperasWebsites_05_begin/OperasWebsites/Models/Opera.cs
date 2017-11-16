f8dae0abe6ec03bae094782e91e5d507c426a3e5
Baruch Toledano <barucht@sela.co.il>
2017-11-16 15:52:35 +0200

    Update Opera.cs
    Fixed bug: #83712

--- Allfiles/20486c/Mod05/Democode/OperasWebsites_05_begin/OperasWebsites/Models/Opera.cs
+++ Allfiles/20486c/Mod05/Democode/OperasWebsites_05_begin/OperasWebsites/Models/Opera.cs
@@ -11,10 +11,14 @@ namespace OperasWebsites.Models
         [Required]
         public int OperaID { get; set; }
         [StringLength(200)]
+        [CheckValidTitle]
+        [Required]
         public string Title { get; set; }
         [CheckValidYear]
         [Required]
         public int Year { get; set; }
+        [CheckValidComposer]
+        [Required]
         public string Composer { get; set; }
     }
 
@@ -36,6 +40,46 @@ namespace OperasWebsites.Models
         public CheckValidYear()
         {
             ErrorMessage = "The earliest opera is Daphne, 1598, by Corsi, Peri, and Rinuccini";
+        }
+    }
+
+    public class CheckValidTitle : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string title = (string)value;
+            if (title.Length < 3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public CheckValidTitle()
+        {
+            ErrorMessage = "The title must contain at least 3 characters";
+        }
+    }
+
+    public class CheckValidComposer : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string composer = (string)value;
+            if (composer.Length < 4)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public CheckValidComposer()
+        {
+            ErrorMessage = "The composer must contain at least 4 characters";
         }
     }
 }
