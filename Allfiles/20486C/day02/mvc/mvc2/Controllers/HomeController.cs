73d55b21f58a360c81ba5bbcebd79ecb537df2b3
Tomasz Kołodziejczyk <xanderekpl@gmail.com>
2019-01-08 10:25:23 +0100

    changes

---
+++ Allfiles/20486C/day02/mvc/mvc2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Info()
+        {
+            var homeInfo = typeof(HomeController);
+            Response.Write($"{homeInfo.BaseType}\n");
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
