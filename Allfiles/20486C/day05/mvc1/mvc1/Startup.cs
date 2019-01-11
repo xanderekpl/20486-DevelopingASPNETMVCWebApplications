5f1e8596a0d71b8eb83fb73d6fb3c78efb453a6b
Tomasz Kołodziejczyk <xanderekpl@gmail.com>
2019-01-11 15:26:33 +0100

    day last

---
+++ Allfiles/20486C/day05/mvc1/mvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc1.Startup))]
+namespace mvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
